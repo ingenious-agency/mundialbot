a4d54891504e64d7e6a950d38b6fc55aa3622f78
Gabriel Chertok <gchertok@gmail.com>
2018-06-13 18:36:59 -0300

    Change the filter to use the correct status

--- src/footbal.js
+++ src/footbal.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getPlayingGames = async () => {
   const response = await axios.get('http://api.football-data.org/v1/competitions/467/fixtures');
-  return response.data.fixtures.filter(fixture => fixture.status === "TIMED");
+  return response.data.fixtures.filter(fixture => fixture.status === "IN_PLAY");
 };
 
 const isTeamPlaying = async (team) => {
